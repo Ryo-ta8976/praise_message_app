38f986713f8ce03746883bd7e937116c567847b6
Ryo-ta8976 <0vr6551b2mb422g@gmail.com>
2020-05-28 09:13:37 +0900

    拍手ボタン作成

--- src/SimpleCard.jsx
+++ src/SimpleCard.jsx
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import ImageAvatar from './ImageAvatar';
 
@@ -56,7 +55,7 @@ export default function SimpleCard() {
         </div>
       </CardContent>
       <CardActions>
-        <Button size="small">Learn More</Button>
+        <input type="image" src="/static/images/other/applause_icon.png" />
       </CardActions>
     </Card>
   );
